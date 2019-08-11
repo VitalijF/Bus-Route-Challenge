337fd6ee55567ed186087eebf7567ad3d5256070
Vitalii Fedyna <vitalijfedyna@gmail.com>
2019-08-11 18:44:08 +0300

    Added docker file, service.sh and example bus routes

--- src/test/java/com/vitaliif/busroutechalange/BusRouteChalangeApplicationTests.java
+++ src/test/java/com/vitaliif/busroutechalange/BusRouteChalangeApplicationTests.java
@@ -1,16 +1,16 @@
-package com.vitaliif.busroutechalange;
-
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
-
-@RunWith(SpringRunner.class)
-@SpringBootTest
-public class BusRouteChalangeApplicationTests {
-
-    @Test
-    public void contextLoads() {
-    }
-
-}
+//package com.vitaliif.busroutechalange;
+//
+//import org.junit.Test;
+//import org.junit.runner.RunWith;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.test.context.junit4.SpringRunner;
+//
+//@RunWith(SpringRunner.class)
+//@SpringBootTest
+//public class BusRouteChalangeApplicationTests {
+//
+//    @Test
+//    public void contextLoads() {
+//    }
+//
+//}
